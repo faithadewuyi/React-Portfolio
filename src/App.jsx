9d2f35cc0e0f38e70b79a5ea10c8e9bf026ad188
faithadewuyi <iyanuleke09@gmail.com>
2024-01-22 23:34:27 +0100

    chore: Sidenav and hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./app.scss"
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/hero/Hero"
 const App = () => {
   return <div>
-    <section>
+    <section id="Home">
       <Navbar/>
+      <Hero/>
     </section>
-    <section>About</section>
-    <section>Services</section>
-    <section>Projects1</section>
-    <section>Project2</section>
+    <section id="About">About</section>
+    <section id="Skills">Services</section>
+    <section id="Projects">Projects1</section>
+    <section id="Contact">Project2</section>
     <section>Project3</section>
     <section>Contact</section>
   </div>;
---
+++ src/Components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+
+import "./hero.scss"
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="imageContainer">
+        <img src="heroo.png"></img>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
